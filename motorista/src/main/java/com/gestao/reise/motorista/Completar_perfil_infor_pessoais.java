5a9a37520fb7652b60d3e7529a84531cb4debdf0
cainanalves <cainan.teixeira10@hotmail.com>
2017-09-02 20:55:39 -0300

    activity completar perfil

---
+++ motorista/src/main/java/com/gestao/reise/motorista/Completar_perfil_infor_pessoais.java
@@ -0,0 +1,13 @@
+package com.gestao.reise.motorista;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Completar_perfil_infor_pessoais extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_completar_perfil_infor_pessoais);
+    }
+}
