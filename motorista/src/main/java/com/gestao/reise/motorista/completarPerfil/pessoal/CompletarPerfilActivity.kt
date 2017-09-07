d6440fdb3574e20e71397c99d7ebaf33f34d182c
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-09-07 12:52:27 -0300

    Alterações no layout das telas do modulo passageiro

---
+++ motorista/src/main/java/com/gestao/reise/motorista/completarPerfil/pessoal/CompletarPerfilActivity.kt
@@ -0,0 +1,13 @@
+package com.gestao.reise.motorista.completarPerfil.pessoal
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by arthur on 07/09/17.
+ */
+class CompletarPerfilActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
