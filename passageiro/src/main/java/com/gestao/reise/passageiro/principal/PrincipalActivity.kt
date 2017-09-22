1a226f6da22f8eac3f619538d33f9e989c10bf75
ArthurGorgonio <gorgonioarthur@gmail.com>
2017-09-22 11:46:13 -0300

    Menu Drawer

--- passageiro/src/main/java/com/gestao/reise/passageiro/principal/PrincipalActivity.kt
+++ passageiro/src/main/java/com/gestao/reise/passageiro/principal/PrincipalActivity.kt
@@ -1,16 +1,47 @@
 package com.gestao.reise.passageiro.principal
 
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
+import android.view.Menu
+import android.view.MenuItem
 import com.gestao.reise.passageiro.BaseActivity
 
 import com.gestao.reise.passageiro.R
+import kotlinx.android.synthetic.main.activity_principal.*
+import kotlinx.android.synthetic.main.app_bar_principal.*
 
-class PrincipalActivity : BaseActivity() {
+class PrincipalActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_principal)
 
+        setSupportActionBar(toolbar)
+
         showProgress()
+
+        val toggle = ActionBarDrawerToggle(this, drawer_layout, toolbar, R.string.drawer_open, R.string.drawer_close)
+
+        drawer_layout.setDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_drawer.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
